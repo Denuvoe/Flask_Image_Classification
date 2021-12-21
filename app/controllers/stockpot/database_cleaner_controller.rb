bd3d76b2639974de1acc315dc1ade58f2309c2a9
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2021-12-21 20:14:09 +0000

    Update travis settings

--- app/controllers/stockpot/database_cleaner_controller.rb
+++ app/controllers/stockpot/database_cleaner_controller.rb
@@ -3,4 +3,5 @@
 module Stockpot
   class DatabaseCleanerController < MainController
     # Clean database before, between, and after tests by clearing Rails
-    # and REDIS caches and truncat
+    # and REDIS caches and truncating the active record database.
+    d
