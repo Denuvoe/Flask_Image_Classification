ed31de7346487f950e6c0c3d9895b74d6c82c55e
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-01-14 15:32:36 +0000

    auto -- updated api data json files

--- app/controllers/stockpot/redis_controller.rb
+++ app/controllers/stockpot/redis_controller.rb
@@ -3,3 +3,34 @@
 
 module Stockpot
   class RedisController < MainController
+    def index
+      if params[:field].present?
+        # Returns the value associated with :field in the hash stored at :key
+        record = REDIS.hget(params[:key], params[:field])
+      else
+        # Returns the value of :key
+        record = REDIS.get(params[:key])
+      end
+
+      render json: record.to_json, status: :ok
+    end
+
+    def create
+      if params[:field].present?
+        # Sets :field in the hash stored at :key to :value
+        REDIS.hset(params[:key], params[:field], params[:value])
+      else
+        # Sets :key to hold the string :value
+        REDIS.set(params[:key], params[:value])
+      end
+
+      render json: { status: 201 }
+    end
+
+    def keys
+      record = REDIS.keys
+
+      render json: record.to_json, status: :ok
+    end
+  end
+end
