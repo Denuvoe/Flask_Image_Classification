b6f6578be570b323756b26c49c0bfbe759e4ad4f
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-02-13 12:30:39 +0000

    Updated npmignore

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@
 Stockpot::Engine.routes.draw do
   get "/records", to: "records#index"
   post "/records", to: "records#create"
-  delete "/records", to: "records
+  delete "/records", to: "records#destroy"
+  put "/records", to: "records#update"
+
+  
