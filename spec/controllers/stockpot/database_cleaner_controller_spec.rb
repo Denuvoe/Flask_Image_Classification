568075c572f0115e9619591d505c7a70fe07310d
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-03-01 07:34:11 +0000

    Update Change Log

--- spec/controllers/stockpot/database_cleaner_controller_spec.rb
+++ spec/controllers/stockpot/database_cleaner_controller_spec.rb
@@ -3,3 +3,32 @@
 
 RSpec.describe Stockpot::DatabaseCleanerController, type: :request do
   let(:body) { { status: 204 }.to_json }
+
+  describe "DELETE #index" do
+    it "truncates the database" do
+      FactoryBot.create(:user)
+      expect(User.all.count).to eq(1)
+      delete clean_database_path
+      expect(User.all.count).to eq(0)
+      expect(response.body).to eq(body)
+    end
+
+    it "flushes REDIS" do
+      REDIS.set("test", "test")
+      expect(REDIS.keys).not_to be_empty
+      delete clean_database_path
+      expect(REDIS.keys).to be_empty
+      expect(response.body).to eq(body)
+    end
+
+    it "restores the server time back to normal" do
+      year = 1989
+
+      Timecop.freeze(Time.zone.local(year))
+      expect(Time.zone.now.year).to eq(year)
+      delete clean_database_path
+      expect(Time.zone.now.year).not_to eq(year)
+      expect(response.body).to eq(body)
+    end
+  end
+end
