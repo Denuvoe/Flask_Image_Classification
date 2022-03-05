bec05a5569034adef9de239d9ed421ce203c0421
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-03-05 23:00:27 +0000

    Fixed #15

--- spec/controllers/stockpot/healthz_controller_spec.rb
+++ spec/controllers/stockpot/healthz_controller_spec.rb
@@ -2,4 +2,5 @@
 
 RSpec.describe Stockpot::HealthzController, type: :request do
   describe "GET #index" do
-    it "returns successfully if the route i
+    it "returns successfully if the route is available" do
+      get heal
