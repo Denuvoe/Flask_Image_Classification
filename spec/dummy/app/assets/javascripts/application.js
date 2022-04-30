71bdccd5bc5febf00f4c275ea92f6dee98f1ae52
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-04-30 08:30:52 +0000

    Add server

--- spec/dummy/app/assets/javascripts/application.js
+++ spec/dummy/app/assets/javascripts/application.js
@@ -5,4 +5,4 @@
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. Ja
+// compiled file. JavaScript code in this file should be add
