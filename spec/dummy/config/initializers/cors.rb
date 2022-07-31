cf69c118e6f002c3225d57553b07aef8fe78c79f
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-07-31 15:57:00 +0000

    Fix uninitialized variable

--- spec/dummy/config/initializers/cors.rb
+++ spec/dummy/config/initializers/cors.rb
@@ -3,4 +3,4 @@
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX req
