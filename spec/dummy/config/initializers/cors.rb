6d6b2f9ce9eafcc3e27bd3de39f6d51229b7f16f
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-08-04 09:54:00 +0000

    Made some changes

--- spec/dummy/config/initializers/cors.rb
+++ spec/dummy/config/initializers/cors.rb
@@ -3,4 +3,6 @@
 # Be sure to restart your server when you modify this file.
 
 # Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX req
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+
+# Read more: https://
