c867339f9224c21edebf30bef7242025998c6e7f
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-08-05 08:14:17 +0000

    Fix travis syntax

--- spec/dummy/config/initializers/cors.rb
+++ spec/dummy/config/initializers/cors.rb
@@ -5,4 +5,5 @@
 # Avoid CORS issues when API is called from the frontend app.
 # Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
 
-# Read more: https://
+# Read more: https://github.com/cyu/rack-cors
+
