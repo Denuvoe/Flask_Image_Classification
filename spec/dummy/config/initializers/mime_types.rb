e912717cb4a95f3a143fa5e951ae68fe151d9c1a
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-08-09 22:00:16 +0000

    updating latest changes...

--- spec/dummy/config/initializers/mime_types.rb
+++ spec/dummy/config/initializers/mime_types.rb
@@ -2,4 +2,5 @@
 
 # Be sure to restart your server when you modify this file.
 
-# Add new mime types for use in respond_to block
+# Add new mime types for use in respond_to blocks:
+# Mime::Type.register "text/richtext"
