d9b108ba96307503c4ff37bfb27f619d91ee6902
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-08-16 16:36:34 +0000

    mac80211: fix use of uninitialised values in RX aggregation

--- spec/dummy/config/initializers/wrap_parameters.rb
+++ spec/dummy/config/initializers/wrap_parameters.rb
@@ -2,4 +2,7 @@
 
 # Be sure to restart your server when you modify this file.
 
-# This file contains settings for ActionController::
+# This file contains settings for ActionController::ParamsWrapper which
+# is enabled by default.
+
+# E
