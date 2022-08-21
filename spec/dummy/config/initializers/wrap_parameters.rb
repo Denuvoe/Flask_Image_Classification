f32a80174c83152df8841b234aed78d5526c9908
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-08-21 11:49:33 +0000

    Release version 1.0.1

--- spec/dummy/config/initializers/wrap_parameters.rb
+++ spec/dummy/config/initializers/wrap_parameters.rb
@@ -5,4 +5,5 @@
 # This file contains settings for ActionController::ParamsWrapper which
 # is enabled by default.
 
-# Enable parameter wrapping for JSON. You can disable this by setting :format to an empty arra
+# Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
+ActiveSupport.on_load(:action_controller) 
