7b2b102c857a868a6bbaea2dc5045852607cc61f
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-08-21 15:25:30 +0000

    pot/merge.

--- spec/dummy/config/initializers/wrap_parameters.rb
+++ spec/dummy/config/initializers/wrap_parameters.rb
@@ -6,4 +6,5 @@
 # is enabled by default.
 
 # Enable parameter wrapping for JSON. You can disable this by setting :format to an empty array.
-ActiveSupport.on_load(:action_controller) 
+ActiveSupport.on_load(:action_controller) do
+  wrap_parameters format: %i[
