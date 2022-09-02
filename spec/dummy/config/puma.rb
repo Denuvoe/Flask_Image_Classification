d9f02c5c74cbef5ac48c895ca55c9b9cb3ae55c7
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-09-02 22:39:22 +0000

    Update all dependencies

--- spec/dummy/config/puma.rb
+++ spec/dummy/config/puma.rb
@@ -3,4 +3,5 @@
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
-# the maximum value specified for Puma. Default is set to
+# the maximum value specified for Puma. Default is set to 5 threads for minimum
+# and maximum;
