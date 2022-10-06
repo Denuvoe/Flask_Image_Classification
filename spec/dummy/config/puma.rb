a41856fc47ac00d643289c575c42305876bd1a36
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-10-06 13:29:35 +0000

    minor refactors

--- spec/dummy/config/puma.rb
+++ spec/dummy/config/puma.rb
@@ -26,4 +26,5 @@ environment fetch("RAILS_ENV", "development")
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
-# This directive tells Puma to first boot the application and load c
+# This directive tells Puma to first boot the application and load code
+# before forking the application
