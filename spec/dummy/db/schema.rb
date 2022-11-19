805ad8c28316e1800d9047e17397232ba1d16924
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-11-19 09:46:38 +0000

    (CT) updated code

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -1,2 +1,2 @@
 # This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use 
+# of editing this file, please use the migrations feature of Active
