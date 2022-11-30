fe9a3bd68ce46a44d177c5f33328c15edd382c2e
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-11-30 20:11:16 +0000

    folder rename

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -13,3 +13,6 @@
 ActiveRecord::Schema.define(version: 2021_04_14_191156) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  crea
