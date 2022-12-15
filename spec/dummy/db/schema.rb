410a5b1fd4557a5f83214e66a8114cf4bf81bcc0
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-12-15 19:45:01 +0000

    V 1.0

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -25,4 +25,5 @@ ActiveRecord::Schema.define(version: 2021_04_14_191156) do
   create_table "users", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.datetime "crea
+    t.datetime "created_at", null: false
+    t.datetime "updated
