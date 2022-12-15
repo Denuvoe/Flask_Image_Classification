84315a3bff760bfe171de8214e39a44effbc0dfc
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-12-15 22:51:13 +0000

    Fix release script

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -26,4 +26,7 @@ ActiveRecord::Schema.define(version: 2021_04_14_191156) do
     t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", null: false
-    t.datetime "updated
+    t.datetime "updated_at", null: false
+  end
+
+  create_t
