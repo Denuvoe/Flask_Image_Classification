cded6e18eb94a389ae24a0e157a2f7133ba80f3d
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-12-15 15:51:12 +0000

    crypto: algif_hash - Only export and import on sockets with data

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -24,4 +24,5 @@ ActiveRecord::Schema.define(version: 2021_04_14_191156) do
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
- 
+    t.string "last_name"
+    t.datetime "crea
