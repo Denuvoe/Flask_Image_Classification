48eddfda42e46dddf15ab807329a5cc028de4ddb
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-12-18 07:07:17 +0000

    updated for new release

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -34,4 +34,4 @@ ActiveRecord::Schema.define(version: 2021_04_14_191156) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["user
+    t.index ["user_id"], name: "index_users_admins_on_user
