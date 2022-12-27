fd49b4d4ebe6c6df3b3eb2d7c536a2690a07a82d
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2022-12-27 22:56:08 +0000

    Initial project structure

--- spec/dummy/spec/factories/users/admin.rb
+++ spec/dummy/spec/factories/users/admin.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :users_admin, class: "Users::Admin"
+  factory :users_admin, class: "Users::Admin" do
+    is_admin { true }
+    association(:user, 
