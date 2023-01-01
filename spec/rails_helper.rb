c751edbbf940d1788a5c6f9ee57258757481efe6
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2023-01-01 20:52:58 +0000

    ignore file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,4 +9,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('dummy/config/environment', __dir__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-requir
+require 'rspec/rails'
+
+RSpec.configure 
