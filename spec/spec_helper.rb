444522caa1083bdd762d30fd6970af9afd190f93
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2023-01-13 06:45:34 +0000

    CF 1.5

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,3 +20,22 @@ RSpec.configure do |config|
     DatabaseCleaner[:active_record].strategy = :transaction
     DatabaseCleaner[:redis].strategy = :deletion
   end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
