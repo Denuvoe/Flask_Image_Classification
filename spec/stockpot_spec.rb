ac1920d558c4b103238dbf2ea396b5125e469dc1
Denuvoe <107293108+Denuvoe@users.noreply.github.com>
2023-01-19 07:02:10 +0000

    gpio: unregister gpiochip device before removing it

--- spec/stockpot_spec.rb
+++ spec/stockpot_spec.rb
@@ -3,4 +3,6 @@
 RSpec.describe Stockpot do
   it "has a version number" do
     expect(described_class::VERSION).to be_a String
-    expect(described_class::VERSION).not_to
+    expect(described_class::VERSION).not_to be_blank
+  end
+end
